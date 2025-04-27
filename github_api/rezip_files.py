eb4c395687aa991c8167451709524ecb187508f5
df1000 <dino.567.sour@gmail.com>
2025-04-27 16:18:18 +0200

    extract source code from test repo

--- github_api/rezip_files.py
+++ github_api/rezip_files.py
@@ -2,10 +2,11 @@ import zipfile
 import os
 
 # Path to the original ZIP file
-original_zip = "../data/repo_data/taniiishk_rock-paper-scissors-game_2025-04-20_18-35-28.zip"
+original_zip = "../data/raw_repo_data/abi_screenshot-to-code_2025-04-21_09-10-25.zip"
+
 
 # Directory to extract the contents
-extracted_dir = "../data/raw_data/"
+extracted_dir = "../data/repo_unzip/"
 
 # Extract all files
 with zipfile.ZipFile(original_zip, 'r') as zip_ref:
